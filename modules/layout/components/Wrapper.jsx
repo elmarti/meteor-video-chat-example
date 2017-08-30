cdb61b809fec9291fe33eb56c1f2dc254b81cbf2
jerp dertin <jack@pebblepad.co.uk>
2017-08-30 12:57:53 +0100

    add media error handling

--- modules/layout/components/Wrapper.jsx
+++ modules/layout/components/Wrapper.jsx
@@ -7,14 +7,43 @@ const {Content} = Layout;
 import {Header, Footer} from '../';
 import { CallUsers } from '../../users';
 const confirm = Modal.confirm;
+const error = Modal.error;
+const info = Modal.info;
 
 class Wrapper extends React.Component{
     constructor(){
         super();
 
         Meteor.VideoCallServices.RTCConfiguration = {"iceServers":[{urls:'stun:stun.l.google.com:19302'}]};
-        Meteor.VideoCallServices.onError = (err, data)=>{
-            console.log(err, data);
+        Meteor.VideoCallServices.onError = (err, data) => {
+            switch ( err.name ) {
+                case "NotFoundError":
+                    error({
+                        title : "Could not find webcam",
+                        content : "Please ensure a webcam is connected",
+                        okText : "OK"
+                    });
+                    Meteor.VideoCallServices.endPhoneCall();
+                    break;
+                case "NotReadableError":
+                    error({
+                        title : "Hardware error",
+                        content: "Could not access your device.",
+                        okText : "OK"
+                    });
+                    Meteor.VideoCallServices.endPhoneCall();
+                    break;
+                case "SecurityError":
+                    error({
+                        title : "Security error",
+                        content: "Media support is disabled in this browser.",
+                        okText : "OK"
+                    });
+                    Meteor.VideoCallServices.endPhoneCall();
+                    break;
+                default:
+                    console.log(err, data);
+            }
         };
         Meteor.VideoCallServices.onReceivePhoneCall = (_id) => {
             this.setState({
@@ -31,6 +60,12 @@ class Wrapper extends React.Component{
                 onCancel() {
                     Meteor.VideoCallServices.endPhoneCall();
                 },
+            });
+        };
+        Meteor.VideoCallServices.onTerminateCall = () => {
+            Modal.info({
+               title : "Call ended",
+                okText : "OK"
             });
         };
         this.state = {
